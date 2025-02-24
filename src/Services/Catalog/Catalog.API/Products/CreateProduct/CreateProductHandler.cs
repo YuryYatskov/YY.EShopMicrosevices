b29d4265e8f52a056e1520bb442f1ecf575188a7
Yury Yatskov <Y.Yatskov@hotmail.com>
2025-02-24 12:41:29 +0200

    Add ICommand, IQuery, CreateProductEndpoint, CreateProductHandler, global using, Carter, MediatR.

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,35 @@
+﻿namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price)
+    : ICommand<CreateProductResault>;
+
+public record CreateProductResault(Guid Id);
+
+internal class CreateProductHandler(IDocumentSession session)
+    : ICommandHandler<CreateProductCommand, CreateProductResault>
+{
+    public async Task<CreateProductResault> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        // Create product entity from command object.
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price,
+        };
+
+        // Save to database.
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+
+        // Return CreateProductResault result.
+        return new CreateProductResault(product.Id);
+    }
+}
