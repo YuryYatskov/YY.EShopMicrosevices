c209aa89481bf4c586ee2791a5433e7cb8edddc4
Yury Yatskov <Y.Yatskov@hotmail.com>
2025-03-07 13:54:19 +0200

    Page list with page number and page size.

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -1,19 +1,23 @@
-﻿
-namespace Catalog.API.Products.GetProducts;
+﻿namespace Catalog.API.Products.GetProducts;
 
+public record GetProductsRequest(int? PageNumber = 1, int? PageSize = 10);
 public record GetProductsResponse(IEnumerable<Product> Products);
 
 public class GetProductsEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapGet("/products", async (ISender sender) =>
+        app.MapGet("/products", async (
+            [AsParameters] GetProductsRequest request,
+            ISender sender) =>
         {
-            var result = await sender.Send(new GetProductsQuery());
+            var query = request.Adapt<GetProductsQuery>();
 
-            var products = result.Adapt<GetProductsResponse>();
+            var result = await sender.Send(query);
 
-            return Results.Ok(products);
+            var response = result.Adapt<GetProductsResponse>();
+
+            return Results.Ok(response);
         })
         .WithName("GetProducts")
         .Produces<GetProductsResponse>(StatusCodes.Status200OK)
