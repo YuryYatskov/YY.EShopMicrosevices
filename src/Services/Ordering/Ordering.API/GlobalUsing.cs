b3ade3917d8ba3570ec40803d52fc75d6c663191
Yury Yatskov <Y.Yatskov@hotmail.com>
2025-03-28 16:32:08 +0200

    Ordering domain.

---
+++ src/Services/Ordering/Ordering.API/GlobalUsing.cs
@@ -0,0 +1,5 @@
+﻿global using Mapster;
+global using MediatR;
+global using Ordering.API;
+global using Ordering.Application;
+global using Ordering.Infrastructure;
---
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -0,0 +1,10 @@
+﻿global using BuildingBlocks.CQRS;
+global using MediatR;
+global using Microsoft.Extensions.Logging;
+global using BuildingBlocks.Behaviors;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using System.Reflection;
+global using Ordering.Domain.Abstractions;
+global using Ordering.Domain.Enums;
+global using Ordering.Domain.ValueObjects;
