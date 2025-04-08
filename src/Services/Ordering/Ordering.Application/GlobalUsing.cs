2de8732cc2f2a848d99c48082a440aca2e6742f7
Yury Yatskov <Y.Yatskov@hotmail.com>
2025-04-08 14:41:28 +0300

    Create Order entity from command object. IApplicationDbContext. OrderDto.

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -1,4 +1,12 @@
 ﻿global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.CQRS;
+global using FluentValidation;
+global using Microsoft.EntityFrameworkCore;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
+global using Ordering.Application.Data;
+global using Ordering.Application.Dtos;
+global using Ordering.Domain.Enums;
+global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
 global using System.Reflection;
--- src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
@@ -6,6 +6,7 @@ global using Microsoft.EntityFrameworkCore.Diagnostics;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
+global using Ordering.Application.Data;
 global using Ordering.Domain.Abstractions;
 global using Ordering.Domain.Enums;
 global using Ordering.Domain.Models;
